5fb33ace6e1549b3e64f72227ab6559b28cc7b0e
Ahq <hey.yadong@gmail.com>
2020-10-13 13:12:23 +0800

    deploy to github pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+let Index = () => (
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Index />, rootElement);
+} else {
+  render(<Index />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
